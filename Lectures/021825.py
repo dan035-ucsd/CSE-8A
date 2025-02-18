8f4a88e058a117f897d809a12510ac857dfe7c66
Dani <dan035@ucsd.edu>
2025-02-18 13:37:35 -0800

    Reapply "add files via upload"
    This reverts commit 58f69a1aa92c4dac8aa3641555100093f0ccd53f.

--- Lectures/021825.py
+++ Lectures/021825.py
@@ -1,9 +1,30 @@
 import csv
-data = list(csv.reader(open(college_hs_wages.csv)))
-hs_wages = get_hs_wages(data)
+data = list(csv.reader(open("college_hs_wages.csv")))
 
-def get_hs_wages(all_data):
-    wages = []
+
+def get_col_data(all_data,col_num):
+    targetData = []
     for row in range(1,len(all_data)):
-        wages.append(row[1])
-    return wages
+        targetData.append(all_data[row][col_num])
+    return targetData
+
+def group_by_rating(song_data):
+    grouped_songs = {}
+    for song in song_data:
+        rating = song[2]
+        if rating in grouped_songs:
+            grouped_songs[rating].append(song[1])
+        else:
+            grouped_songs[rating] = [song[1]]
+    return grouped_songs
+
+song_data = [['Movie', 'Song Name', 'Rating'], 
+             ['Aladdin', 'Friend Like Me', '8'], 
+             ['Beauty and the Beast', ' Gaston', '2'],  
+             ['Moana', "How Far I'll Go", '8'],
+             ['The Little Mermaid', 'Under the Sea', '8'],
+             ['The Lion King', 'Circle of Life', '2']]
+
+grouped_songs = group_by_rating(song_data[1:])
+print(grouped_songs)
+
