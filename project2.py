ea740494426ae3c4fdd57528ce5db7ff2b7c652c
Dani <dan035@ucsd.edu>
2025-02-19 00:19:25 -0800

    add files via upload

--- project2.py
+++ project2.py
@@ -4,3 +4,9 @@
 
 import csv
 data = list(csv.reader(open("Ed Sheeran Dataset.csv")))
+
+def average_col(col): #6, 8, 9, 11.
+    total = 0
+    for row in data[1:]:
+        total += int(row[col])
+    return total / len(data[1:])
