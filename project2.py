f55c6c774cbb022cb9f58151af7b234c7653cd15
HOTBOX <dangorochimaru@gmail.com>
2025-02-25 13:37:23 -0800

    add files via upload

--- project2.py
+++ project2.py
@@ -1,36 +1,29 @@
 ### CSE 8A Project 2
 ### Author: Dani Nguyen
-### Collaborations:
+### Collaborations: Github Copilot
+
+# Questions
+# 1. What are Ed Sheeran's top 3 most danceable releases?
+# 2. What are the top 3 most popular releases of the year 2017?
+# 3. 
 
 import csv
 data = list(csv.reader(open("Ed Sheeran Dataset.csv")))
 
-# how popular is each album
+#   num vars                    catergorical vars
+    # 6 = popularity            # 3 = album_name
+    # 8 = danceability          # 7 = explicit 
+    # 9 = energy                # 10 = key
+    # 11 = loudness             # 12 = mode
+    # 13 = speechiness          # 19 = time_signature
+    # 14 = acousticness         # 20 = featured_artists
+    # 15 = instrumentalness     # 21 = type
+    # 16 = liveness             # 23
+    # 17 = valence              
+    # 18 = tempo                
 
-def average_col(col): # averages a column
-    # 6 = popularity
-    # 8 = danceability
-    # 9 = energy
-    # 11 = loudness
-    # 13 = speechiness
-    # 14 = acousticness
-    # 15 = instrumentalness
-    # 16 = liveness
-    # 17 = valence
-    # 18 = tempo
-    total = 0
-    for row in data[1:]:
-        total += int(row[col])
-    return total / len(data[1:])
-
-def create_dict(col): # creates a tally of the column in dict form
-    # 3 = album_name
-    # 7 = explicit 
-    # 10 = key 
-    # 12 = mode
-    # 19 = time_signature
-    # 20 = featured_artists
-    # 21 = type
+def create_dict(col): 
+    ''' Given a column, returns dictionary which is a tally of said column'''
     d = {}
     for row in data[1:]:
         entry = row[col]
@@ -40,25 +33,37 @@ def create_dict(col): # creates a tally of the column in dict form
             d[entry] += 1
     return d
 
-def avg_col_perAlbum(col, album): # averages a column with album filter
+def avg_col_perAlbum(col, album): 
+    ''' Given a column of interest, returns an average of said column only for a specified album '''
     total = 0
     count = 0
     lst = []
-    
     for row in data[1:]:         # filters column
         if row[3] == album:
             lst.append(row)
     for row in lst:              # averages column
-        total += int(row[col])
+        total += float(row[col])
         count += 1
     return total / count
 
-def avg_col_byAlbum(col):    # averages a column for every album
+def avg_col_byAlbum(col):    
+    ''' Given a column of interest, returns a list of tuples with the average of that column, 
+    which is calculated using the function avg_col_perAlbum() and create_dict() '''
     albums = list(create_dict(3).items())
     lst = []
-    for (key, val) in albums:
-        lst.append((avg_col_perAlbum(col,key),key)) 
-    lst.sort(reverse=True)
+    for (key, val) in albums:                             # creates a sorted list of tuples 
+        lst.append((avg_col_perAlbum(col,key),key))       # with column averages on the left, 
+    lst.sort(reverse=True)                                # sorted in descending order,
+    return lst                                            # and album names on the right.                
+
+def avg_col_perYear(col, year):
+    total = 0
+    count = 0
+    lst = []
+    for row in data[1:]:
+        if row[-3] == str(year):
+            lst.append(row)                        
     return lst
 
-print(avg_col_byAlbum(6)[0])
+print("Ed Sheeran's top 3 most danceable releases are " + avg_col_byAlbum(8)[0][1] +  
+      ", " + avg_col_byAlbum(8)[1][1] + ", and " + avg_col_byAlbum(8)[2][1] + ".")
