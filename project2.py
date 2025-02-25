5c66bb8ec02f219ecc07ad8ab0fa850bde7a6a31
HOTBOX <dangorochimaru@gmail.com>
2025-02-25 12:21:23 -0800

    add files via upload

--- project2.py
+++ project2.py
@@ -4,10 +4,10 @@
 
 import csv
 data = list(csv.reader(open("Ed Sheeran Dataset.csv")))
-# track_id,album_name,album_id,release_date,popularity,explicit,danceability,energy,key,loudness,mode,speechiness,acousticness,instrumentalness,liveness,valence,tempo,time_signature,featured_artists,'type', duration,year,month,day_of_the_week
-# 2        3          4        5            6          7        8            9      10  11       12   13          14           15               16       17     18            19               20      21      22       23   24    25
 
-def average_col(col): #averages a column
+# how popular is each album
+
+def average_col(col): # averages a column
     # 6 = popularity
     # 8 = danceability
     # 9 = energy
@@ -23,7 +23,8 @@ def average_col(col): #averages a column
         total += int(row[col])
     return total / len(data[1:])
 
-def create_dict(col): #creates a dictionary of the column
+def create_dict(col): # creates a tally of the column in dict form
+    # 3 = album_name
     # 7 = explicit 
     # 10 = key 
     # 12 = mode
@@ -34,7 +35,30 @@ def create_dict(col): #creates a dictionary of the column
     for row in data[1:]:
         entry = row[col]
         if entry not in d:
-            d[entry] = 0
+            d[entry] = 1
         else:
             d[entry] += 1
     return d
+
+def avg_col_perAlbum(col, album): # averages a column with album filter
+    total = 0
+    count = 0
+    lst = []
+    
+    for row in data[1:]:         # filters column
+        if row[3] == album:
+            lst.append(row)
+    for row in lst:              # averages column
+        total += int(row[col])
+        count += 1
+    return total / count
+
+def avg_col_byAlbum(col):    # averages a column for every album
+    albums = list(create_dict(3).items())
+    lst = []
+    for (key, val) in albums:
+        lst.append((avg_col_perAlbum(col,key),key)) 
+    lst.sort(reverse=True)
+    return lst
+
+print(avg_col_byAlbum(6)[0])
