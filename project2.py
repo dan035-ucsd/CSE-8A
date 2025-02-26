754f49a64fb83732282b862a50baffcc10133b32
Dani <dan035@ucsd.edu>
2025-02-26 10:48:22 -0800

    add files via upload

--- project2.py
+++ project2.py
@@ -23,7 +23,9 @@ data = list(csv.reader(open("Ed Sheeran Dataset.csv")))
     # 18 = tempo                
 
 def create_dict(col): 
-    ''' Given a column, returns dictionary which is a tally of said column'''
+    ''' 
+    Given a column, returns dictionary which is a tally of said column.
+    '''
     d = {}
     for row in data[1:]:
         entry = row[col]
@@ -33,8 +35,27 @@ def create_dict(col):
             d[entry] += 1
     return d
 
+def avg_col_perCategory(datCol, catCol, filter):     # only works with int categories ..
+    ''' 
+    Given a column, a category, and a filter. Loops through a category column and 
+    a data column, while filtering the category column to a specific item.
+        ex: avg_col_perCategory(6, 3, "Autumn Variations")  returns average popularity of songs on the album
+    '''
+    total = 0
+    count = 0
+    lst = []
+    for row in data[1:]:          
+        if row[catCol] == str(filter):   # filters to only item of interest
+            lst.append(row)          
+    for row in lst:                      # averages column
+        total += float(row[datCol])
+        count += 1
+    return total / count
+
 def avg_col_perAlbum(col, album): 
-    ''' Given a column of interest, returns an average of said column only for a specified album '''
+    ''' 
+    Given a column of interest, returns an average of said column only for a specified album.
+    '''
     total = 0
     count = 0
     lst = []
@@ -47,8 +68,10 @@ def avg_col_perAlbum(col, album):
     return total / count
 
 def avg_col_byAlbum(col):    
-    ''' Given a column of interest, returns a list of tuples with the average of that column, 
-    which is calculated using the function avg_col_perAlbum() and create_dict() '''
+    ''' 
+    Given a column of interest, returns a list of tuples with the average of that column, 
+    which is calculated using the function avg_col_perAlbum() and create_dict().
+    '''
     albums = list(create_dict(3).items())
     lst = []
     for (key, val) in albums:                             # creates a sorted list of tuples 
@@ -56,37 +79,16 @@ def avg_col_byAlbum(col):
     lst.sort(reverse=True)                                # sorted in descending order,
     return lst                                            # and album names on the right.                
 
-def avg_col_perYear(col, year):
-    total = 0
-    count = 0
+def avg_col_byYear(col):
+    years = list(create_dict(23).items())
     lst = []
-    for row in data[1:]:          # filters to only year of interest
-        if row[23] == str(year):
-            lst.append(row)          
-    for row in lst:               # averages column
-        total += float(row[col])
-        count += 1
-    return total / count
-
-def avg_col_perCategory(col, cat, filter):
-    """
-    Given a column, a category, and a filter.
-    Loops through a column of floats, separately loops through a column of category
-    and averages the floats only for those in the filtered category.
-    Ex: avg_col_perCategory(popularity, year, 2022)
-    """
-    total = 0
-    count = 0
-    lst = []
-    for row in data[1:]:          # filters to only year of interest
-        if row[cat] == str(filter):
-            lst.append(row)          
-    for row in lst:               # averages column
-        total += float(row[col])
-        count += 1
-    return total / count
+    for (year, tally) in years:                             # creates a sorted list of tuples 
+        lst.append((avg_col_perCategory(col, 23, year),year))       # with column averages on the left, 
+    lst.sort(reverse=True)                                # sorted in descending order,
+    return lst                                            # and album names on the right.                
 
 print("Ed Sheeran's top 3 most danceable releases are " + avg_col_byAlbum(8)[0][1] +  
       ", " + avg_col_byAlbum(8)[1][1] + ", and " + avg_col_byAlbum(8)[2][1] + ".")
 
-print(avg_col_perCategory(6, 3, "Autumn Variations"))
+print("Ed Sheeran's top 3 years where he had the most popular releases on average are", 
+      avg_col_byYear(6)[0][1] + ", " + avg_col_byYear(6)[1][1] + ", " + avg_col_byYear(6)[2][1])
