72da046685f3fcb835fadefd36e0abd8b08db009
Dani <dan035@ucsd.edu>
2025-02-28 11:20:36 -0800

    add files via upload

---
+++ project3.py
@@ -0,0 +1,16 @@
+### CSE 8A Project 2
+### Author: Dani Nguyen
+### Collaborations: None
+
+from PIL import Image
+
+
+
+# flip image
+img.putpixel(width() - 1 - x, y, color)
+
+# color filter
+
+# rotate image
+
+# crop image
