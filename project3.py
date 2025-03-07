f4ed7b09fe898d06f889652d5f143cf8d4a38614
Dani <dan035@ucsd.edu>
2025-03-06 21:10:38 -0800

    add files via upload

--- project3.py
+++ project3.py
@@ -3,7 +3,11 @@
 ### Collaborations: None
 
 from PIL import Image
+https://pillow.readthedocs.io/en/stable/reference/Image.html
 
+scene = Image.open("scene.jpg")
+shaq = Image.open("shaq.jpg")
+ball = Image.open("ball.jpg")
 
 
 # flip image
@@ -16,3 +20,7 @@ img.putpixel(width() - 1 - x, y, color)
 # crop image
 
 # edge detection
+https://www.geeksforgeeks.org/what-is-edge-detection-in-image-processing/
+
+# overlay images
+https://stackoverflow.com/questions/10640114/overlay-two-same-sized-images-in-python
