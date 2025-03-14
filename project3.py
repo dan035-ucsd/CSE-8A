f592b359cb90d712cfe840d3bec8a2023b95a7a6
Dani <dan035@ucsd.edu>
2025-03-13 18:25:44 -0700

    add files via upload

--- project3.py
+++ project3.py
@@ -3,28 +3,44 @@
 ### Collaborations: None
 
 from PIL import Image
-https://pillow.readthedocs.io/en/stable/reference/Image.html
+# https://pillow.readthedocs.io/en/stable/reference/Image.html
 
 scene = Image.open("scene.jpg")
 shaq = Image.open("shaq.jpg")
 ball = Image.open("ball.jpg")
-out = Image.new("RGB", (scene.width, scene.height))
 
-# flip image
-img.putpixel(width() - 1 - x, y, color)
+scene = scene.resize((1600, 1200))
+
+def flipHoriz(inp):     # flip image horizontally
+    out = Image.new("RGB", (inp.width, inp.height))
+    for x in range(inp.width):
+        for y in range(inp.height):
+            out.putpixel((inp.width - 1 - x, y), inp.getpixel((x, y)))
+    return out
+
+def flipVert(inp):      # flip image vertically
+    out = Image.new("RGB", (inp.width, inp.height))
+    for x in range(inp.width):
+        for y in range(inp.height):
+            out.putpixel((x, inp.height - 1 - y), inp.getpixel((x, y)))
+    return out
+
+def overlay(inp, overlay, xloc, yloc):         # overlay an image
+    out = Image.new("RGB", (inp.width, inp.height))
+    for x in range(xloc, xloc + overlay.width):
+        for y in range(yloc, yloc + overlay.height):
+            for i in range(overlay.width):
+                for j in range(overlay.height):
+                    inp.putpixel((x, y), overlay.getpixel((x - xloc, y - yloc)))
+    
+
 
 # color filter
 
-# rotate image
-
-# crop image
-
 # edge detection
-https://www.geeksforgeeks.org/what-is-edge-detection-in-image-processing/
+# https://www.geeksforgeeks.org/what-is-edge-detection-in-image-processing/
 
 # overlay images
-https://stackoverflow.com/questions/10640114/overlay-two-same-sized-images-in-python
+# https://stackoverflow.com/questions/10640114/overlay-two-same-sized-images-in-python
 
-
-out.Image.show()
-out.save("output.jpg")
+# out.save("output.jpg")
