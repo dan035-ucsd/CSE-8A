59eabb2d8784ca018ca83b4daa12651a85645089
Dani <dan035@ucsd.edu>
2025-03-13 17:15:37 -0700

    add files via upload

--- project3.py
+++ project3.py
@@ -1,4 +1,4 @@
-### CSE 8A Project 2
+### CSE 8A Project 3
 ### Author: Dani Nguyen
 ### Collaborations: None
 
@@ -8,7 +8,7 @@ https://pillow.readthedocs.io/en/stable/reference/Image.html
 scene = Image.open("scene.jpg")
 shaq = Image.open("shaq.jpg")
 ball = Image.open("ball.jpg")
-
+out = Image.new("RGB", (scene.width, scene.height))
 
 # flip image
 img.putpixel(width() - 1 - x, y, color)
@@ -24,3 +24,7 @@ https://www.geeksforgeeks.org/what-is-edge-detection-in-image-processing/
 
 # overlay images
 https://stackoverflow.com/questions/10640114/overlay-two-same-sized-images-in-python
+
+
+out.Image.show()
+out.save("output.jpg")
